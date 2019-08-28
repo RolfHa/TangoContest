fe70a9e2c4f97e86a3416a9fb1e4890ff9b2f58a
basti <54583476+sebastianhenke@users.noreply.github.com>
2019-08-28 11:33:03 +0200

    dateien für db und saveable groß geschrieben

--- index.php
+++ index.php
@@ -45,8 +45,8 @@
 // config datei laden (kann außerhalb des WEB-zugriffs gespeichert werden)
 include_once  'config.php';
 //alle andern klassen laden (dateien müssen den klassennamen haben und im Verzeichnis class liegen)
-//spl_autoload_register(function ($class_name) {include "class" . DIRECTORY_SEPARATOR . $class_name . '.php';});
-spl_autoload_register(function ($class_name) {include "class".  DIRECTORY_SEPARATOR .  strtolower ($class_name) . '.php';});
+spl_autoload_register(function ($class_name) {include "class" . DIRECTORY_SEPARATOR . $class_name . '.php';});
+//spl_autoload_register(function ($class_name) {include "class".  DIRECTORY_SEPARATOR .  strtolower ($class_name) . '.php';});
 
 
 
