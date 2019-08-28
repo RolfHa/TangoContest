1932590cf4e8228cfe22e72b7d963e1fd0cdd712
basti <54583476+sebastianhenke@users.noreply.github.com>
2019-08-28 12:31:48 +0200

    jury getall

--- class/Jury.php
+++ class/Jury.php
@@ -1,5 +1,68 @@
 <?php
 class Jury {
+    private $id;
+    private $vorname;
+    private $nachname;
+
+
+    public function __construct($id, $vorname, $nachname)
+    {
+        $this->id = $id;
+        $this->vorname = $vorname;
+        $this->nachname = $nachname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVorname()
+    {
+        return $this->vorname;
+    }
+
+    /**
+     * @param mixed $vorname
+     */
+    public function setVorname($vorname)
+    {
+        $this->vorname = $vorname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNachname()
+    {
+        return $this->nachname;
+    }
+
+    /**
+     * @param mixed $nachname
+     */
+    public function setNachname($nachname)
+    {
+        $this->nachname = $nachname;
+    }
+
+
+
+
     public static function getById($id){
         $db = DB::connect();
         $sql = "SELECT * FROM jury WHERE ID=$id";
@@ -13,4 +76,24 @@ class Jury {
         );
         return $jury;
     }
+
+    public static function getAll(){
+        $db = DB::connect();
+        $sql = "SELECT * FROM jury WHERE ID=$id";
+        $result = mysqli_query($db, $sql);
+        $jury = array();
+        $i=0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $jury[$i] = new Jury(
+                $row['vorname'],
+                $row['nachname'],
+                $row['id']
+            );
+        }
+        return $jury;
+    }
+
+
+
+
 }
