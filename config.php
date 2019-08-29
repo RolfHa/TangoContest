f5d5234919976dba7927ef975aa4f39ab7243673
Ben <Datanka@blutende-klingen.com>
2019-08-29 09:17:41 +0200

    saveclasses test

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+
+//$stage = "local";
+//$stage = "copy";
+$stage = "live";
+
+switch ($stage){
+    case 'local':
+        define("HOST",'localhost');
+        define("USER",'tango');
+        define("PWD",'tango');
+        define("DB_NAME",'tango');
+        break;
+    case 'copy':
+        define("HOST",'10.101.209.13');
+        define("USER",'tango');
+        define("PWD",'tango');
+        define("DB_NAME",'tangokopie');
+        break;
+    case 'live':
+        define("HOST",'10.101.209.3');
+        define("USER",'tango');
+        define("PWD",'tango');
+        define("DB_NAME",'tango');
+        break;
+    default:
+        define("HOST",'10.101.209.13');
+        define("USER",'tango');
+        define("PWD",'tango');
+        define("DB_NAME",'tangokopie');
+        break;
+}
+
+
